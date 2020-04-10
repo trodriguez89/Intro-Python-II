3d43a707d1d55dbb487212029e5b246c75a2ffba
trodriguez89 <trodriguez89@gmail.com>
2020-04-09 20:42:47 -0700

    mvp day 2

--- src/player.py
+++ src/player.py
@@ -1,7 +1,24 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
 
 class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.items = []
+
+    def take_item(self, item):
+        self.items.append(item)
+        print(f"You picked up {item.name}\n")
+
+    def drop_item(self, item):
+        self.items.remove(item)
+        print(f"You dropped {item.name}")
+
+    def player_items(self):
+        if len(self.items) == 0:
+            print("You don't have any items!")
+        else:
+            for item in self.items:
+                print(f"You currently have {item.name}, here is a brief description: {item.description}")
--- src/room.py
+++ src/room.py
@@ -2,10 +2,25 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description, n_to = None, e_to = None, s_to = None, w_to = None):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.n_to = n_to
-        self.e_to = e_to
-        self.s_to = s_to
-        self.w_to = w_to
+        self.items = []
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
+        
+
+    def list_items(self):
+        if len(self.items) == 0:
+            print("There are no items in here.")
+        else:
+            for item in self.items:
+                print(f"The room contains these items: {item.name}\n")
+
+    def remove_item(self, item):
+        self.items.remove(item)
+
+    def add_item(self, item):
+        self.items.append(item)
